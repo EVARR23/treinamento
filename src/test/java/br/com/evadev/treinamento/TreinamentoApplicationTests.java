abeb8254ac4b6df3f161e2723180ff49a1808eab
EvaReboucas30 <140523579+EvaReboucas30@users.noreply.github.com>
2024-04-28 21:13:05 -0300

    treinamento

---
+++ src/test/java/br/com/evadev/treinamento/TreinamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.evadev.treinamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
